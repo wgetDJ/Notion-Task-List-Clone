5bd18a0ae1f08cd40e75f1691b3bf38618e225ac
wgetdj <dibyajyotimishraiiit@gmail.com>
2021-08-03 00:29:48 +0530

    typo fix

---
+++ resources/js/script.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // let today = document.querySelector("today");
+    // let calander = document.querySelector("calender");
+    // let contentName = document.querySelector("content-name");
+    // let modal = document.querySelector("myModal");
+    // let span = document.querySelector("close");
+    // let newTask = document.querySelector("new-task");
+
+    // newTask.onclick = function() {
+    //     modal.style.display = "block";
+    // }
+    // span.onclick = function() {
+    //     modal.style.display = "none";
+    // }
+    // window.onclick = function(event) {
+    //     if (event.target == modal) {
+    //         modal.style.display = "none";
+    //     }
+    // }
+});
