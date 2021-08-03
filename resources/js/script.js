6aad11686f3e253a33ab012fc062d28757f2f4f4
wgetdj <dibyajyotimishraiiit@gmail.com>
2021-08-03 19:22:19 +0530

    displaying data on screen

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,20 +1,56 @@
-document.addEventListener("DOMContentLoaded", () => {
-    // let today = document.querySelector("today");
-    // let calander = document.querySelector("calender");
-    // let contentName = document.querySelector("content-name");
-    // let modal = document.querySelector("myModal");
-    // let span = document.querySelector("close");
-    // let newTask = document.querySelector("new-task");
+const newTaskBtn = document.querySelector(".new-task");
+const todo = document.querySelector(".todo");
+const doing = document.querySelector(".todo");
+const done = document.querySelector(".todo");
+const newTask = document.querySelector("#task");
+const addBtn = document.querySelector(".addItem");
+const inputForm = document.querySelector(".inputItem");
 
-    // newTask.onclick = function() {
-    //     modal.style.display = "block";
-    // }
-    // span.onclick = function() {
-    //     modal.style.display = "none";
-    // }
-    // window.onclick = function(event) {
-    //     if (event.target == modal) {
-    //         modal.style.display = "none";
-    //     }
-    // }
+let todoTask = [];
+let doingTask = [];
+let doneTask = [];
+
+const showForm = () => {
+    inputForm.style.display = "block";
+}
+
+const hideForm = () => {
+    inputForm.style.display = "none";
+}
+
+const displyTask = (whereToDisply, textValue) => {
+
+    const ul = document.querySelector(whereToDisply);
+
+    const li = document.createElement("li");
+    li.classList.add("item");
+    const listText = document.createTextNode(textValue);
+    li.appendChild(listText);
+
+    ul.appendChild(li);
+}
+
+const addTaskToTaskList = (list, value) => {
+    list.push(value);
+    if (list == todoTask) {
+        displyTask(".todo-list", value);
+    } else if (task == doingTask) {
+        displyTask(".doing-list", value);
+    } else {
+        displyTask(".done-list", value);
+    }
+}
+
+const getTask = () => {
+    let newTaskText = newTask.value;
+    addTaskToTaskList(todoTask, newTaskText);
+}
+
+newTaskBtn.addEventListener("click", function(){
+    showForm();
 });
+
+addBtn.addEventListener("click", function() {
+    getTask();
+    hideForm();
+})
