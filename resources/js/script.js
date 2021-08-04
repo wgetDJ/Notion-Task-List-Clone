8e30ec1fbaace9b15accabbb3f6b710a04736b94
wgetdj <dibyajyotimishraiiit@gmail.com>
2021-08-05 02:44:47 +0530

    adding animation and javascript modificatio

--- resources/js/script.js
+++ resources/js/script.js
@@ -5,10 +5,14 @@ const done = document.querySelector(".todo");
 const newTask = document.querySelector("#task");
 const addBtn = document.querySelector(".addItem");
 const inputForm = document.querySelector(".inputItem");
+// const deleteBtn = document.querySelector(".closebtn");
 
 let todoTask = [];
 let doingTask = [];
 let doneTask = [];
+const allTaskList = [".todo-list", ".doing-list", ".done-list"];
+const listType = ["all-todo", "all-doing", "all-done"];
+
 
 const showForm = () => {
     inputForm.style.display = "block";
@@ -23,20 +27,45 @@ const displyTask = (whereToDisply, textValue) => {
     const ul = document.querySelector(whereToDisply);
 
     const li = document.createElement("li");
-    li.classList.add("item");
+    for (let index in allTaskList) {
+        if (allTaskList[index] == whereToDisply) {
+            const currentType = listType[index];
+            li.classList.add("item", currentType);
+        }
+    }
+
     const listText = document.createTextNode(textValue);
     li.appendChild(listText);
+
     const span = document.createElement("span");
     span.classList.add("closebtn");
+    span.title = "Delete";
     const closeX = document.createTextNode("\u00D7");
     span.appendChild(closeX);
+
     li.appendChild(span);
 
     ul.appendChild(li);
 }
 
+const removeTask = (fromWhereToRemove, textValue) => {
+    for (let index in allTaskList) {
+        if (allTaskList[index] == whereToDisply) {
+            const currentType = listType[index];
+            currentList = document.querySelectorAll(currentType);
+            for (let list of currentList) {
+                console.log(list);
+                list.addEventListener("click", function(){
+                    list.style.display = "none";
+                });
+            }
+        }
+    }
+}
+
 const addTaskToTaskList = (list, value) => {
     list.push(value);
+
     if (list == todoTask) {
         displyTask(".todo-list", value);
     } else if (task == doingTask) {
@@ -44,6 +73,10 @@ const addTaskToTaskList = (list, value) => {
     } else {
         displyTask(".done-list", value);
     }
+}
+
+const clearForm = () => {
+    newTask.value = "";
 }
 
 const getTask = () => {
@@ -57,8 +90,10 @@ newTaskBtn.addEventListener("click", function(){
 
 addBtn.addEventListener("click", function() {
     getTask();
+    clearForm();
     hideForm();
-})
+});
+
 
 
 // ---alert---
@@ -69,5 +104,5 @@ alertCloseBtn.addEventListener("click", function() {
     alertNote.classList.toggle("scaleout");
     setTimeout(function() {
         alertNote.style.display = "none";
-    }, 300);
+    }, 200);
 })
